0b2331bd0730bf49da2a1bcd3c9bc724d6048164
~Alqama <shaikhalqama010@gmail.com>
2026-06-29 02:08:56 +0530

    first commit

---
+++ server/inngest/index.js
@@ -0,0 +1,60 @@
+import { Inngest } from "inngest";
+import prisma from "../configs/prisma.js";
+
+export const inngest = new Inngest({id: "Projectify"});
+
+
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk', triggers: { event: 'clerk/user.created' }},
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data?.email_addresses[0]?.email_address,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            }
+        });
+    }
+);
+
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-with-clerk', triggers: { event: 'clerk/user.deleted' }},
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.delete({
+            where: {
+                id: data.id,
+            }
+        });
+    }
+);
+
+
+// Inngest function to UPDATE user data to the database
+const syncUserUpdation = inngest.createFunction(
+    {id: 'update-user-with-clerk', triggers: { event: 'clerk/user.updated' }},
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.update({
+            where: {
+                id: data.id,
+            },
+            data: {
+                email: data?.email_addresses[0]?.email_address,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.profile_image_url,
+            }
+        });
+    }
+);
+
+//inngest to save workspace member data to the database
+
+
+
+
+export const functions = [syncUserCreation,
+     syncUserDeletion,
+     syncUserUpdation];
---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.get('/', (req, res) => res.send('Server is live!'));
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
